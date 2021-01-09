dcfd7ddf7a9091452ce72c3ae658198fcb97f8b9
adarshbabbula <adarshbabbula@outlook.com>
2021-01-09 21:38:26 +0530

    Learnt Events, Array, Date, Math, condition

---
+++ condition.js
@@ -0,0 +1,19 @@
+a = 5;
+b = 6;
+if(a > b) {
+    console.log(a+ ' is greater than '+ b);
+}
+else {
+    console.log(b+ ' is greater than ' +a);
+}
+switch(a) {
+ case 1: 
+    console.log('value of a is 1');
+    break;
+case 3: 
+    console.log('value of a is 3');
+    break;
+  case 5: 
+    console.log('value of a is 5');
+    break;  
+}
---
+++ math.js
@@ -0,0 +1,4 @@
+console.log(Math.round(4.4));
+console.log(Math.pow(5, 3));
+console.log(Math.max(2, 4, 6, 1, 9));
+console.log(Math.random());
